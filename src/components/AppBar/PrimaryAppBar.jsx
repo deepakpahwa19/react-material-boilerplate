7e94dfc814fad3c171c200d3b5355eb9a7f90330
Deepak Pahawa <deepak.pahawa@dreamorbit.com>
2020-08-30 16:50:39 +0530

    Added eslint and prettier config

--- src/components/AppBar/PrimaryAppBar.jsx
+++ src/components/AppBar/PrimaryAppBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -6,7 +6,6 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
 import HomeIcon from "@material-ui/icons/Home";
-import { CssBaseline } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -34,7 +33,6 @@ export const PrimaryAppBar = React.memo(() => {
 
   return (
     <div className={classes.grow}>
-      <CssBaseline />
       <AppBar position="static">
         <Toolbar>
           <Link to="/" className={classes.title}>
